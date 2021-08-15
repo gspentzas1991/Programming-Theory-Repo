4673a6274b85f54f8432a541573e13d7fe9e1087
Giannis Spentzas <gspentzas1991@gmail.com>
2021-08-15 13:45:46 +0300

    Created a fruit class, with three fruit child classes
    Created a fruit spawner gameobject

---
+++ Programming Theory Project/Assets/Scripts/Apple.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// INHERITANCE -- Apple inherits from fruit
+
+public class Apple : Fruit
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Programming Theory Project/Assets/Scripts/Banana.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// INHERITANCE -- Banana inherits from fruit
+public class Banana : Fruit
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
