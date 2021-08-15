b24c397e6cdf3fac8b920e01573d67e2c753ad40
Giannis Spentzas <gspentzas1991@gmail.com>
2021-08-15 13:17:49 +0300

    Added a DataManager for saving highscore data, along with a scoreModel class

---
+++ Programming Theory Project/Assets/Scripts/DataManager.cs
@@ -0,0 +1,40 @@
+using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+/// <summary>
+/// Handles the saving and loading of data, and keeps temporary data between scenes.
+/// </summary>
+public class DataManager : MonoBehaviour
+{
+    public static string username { get; set; }
+
+    /// <summary>
+    /// Saves in a highscore.json file the username and score of the best scorer
+    /// </summary>
+    public static void  SaveBestScore(ScoreModel score)
+    {
+        string json = JsonUtility.ToJson(score);
+
+        File.WriteAllText(Application.persistentDataPath + "/highscore.json", json);
+    }
+
+    /// <summary>
+    /// Returns the highscore data from a highscore.json file
+    /// </summary>
+    public static ScoreModel GetBestScore()
+    {
+        string path = Application.persistentDataPath + "/highscore.json";
+        var data = new ScoreModel();
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            data = JsonUtility.FromJson<ScoreModel>(json);
+        }
+        return data;
+    }
+
+
+}
